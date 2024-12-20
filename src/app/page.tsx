3571a3e09a43ab9ed454270600c1c47fe2aa64a2
Kavindu401 <kavinduperera2211@gmail.com>
2024-12-20 12:43:12 +0530

    Shadcn import
    ui components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 // import Image from "next/image";
 // import face from "./assets/face.jpg"
 import "./page.css";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
 
 export default function Home() {
   return (
@@ -8,9 +13,38 @@ export default function Home() {
       <div className=" flex items-center h-screen w-[400px]">
         <div className="flex items-center flex-col w-full">
           <h1 className="text-white text-xl">My projects</h1>
-          {/* <div className="border border-white h-12 w-full mt-8 rounded-xl justify-center items-center flex cursor-pointer">
-
-          </div> */}
+          <div className="border border-white h-12 w-full mt-8 rounded-xl justify-center items-center flex cursor-pointer">
+        <HoverCard>
+          <HoverCardTrigger className="text-white">APEX institue payment portal</HoverCardTrigger>
+          <HoverCardContent className="bg-slate-300 text-black">
+            Payment logg portal for APEX INSTITUTE administrators.
+          </HoverCardContent>
+        </HoverCard>
+        </div>
+        <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
+        <HoverCard>
+          <HoverCardTrigger className="text-white">Workout managemant app</HoverCardTrigger>
+          <HoverCardContent className="bg-slate-300 text-black">
+            Android app that helps keep track of personal workouts.
+          </HoverCardContent>
+        </HoverCard>
+        </div>
+        <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
+        <HoverCard>
+          <HoverCardTrigger className="text-white">Music stream app</HoverCardTrigger>
+          <HoverCardContent className="bg-slate-300 text-black">
+            Music streaming app that stream audio throgh youtube.
+          </HoverCardContent>
+        </HoverCard>
+        </div>
+        <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
+        <HoverCard>
+          <HoverCardTrigger className="text-white">Forest Gaurd</HoverCardTrigger>
+          <HoverCardContent className="bg-slate-300 text-black">
+            Music streaming app that stream audio throgh youtube.
+          </HoverCardContent>
+        </HoverCard>
+        </div>
         </div>
       </div>
       <div className="flex flex-col bg-black-500 w-full h-screen">
