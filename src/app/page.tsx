1f576ab9eb341b60036bec320b1db08004d173b8
Kavindu401 <kavinduperera2211@gmail.com>
2024-12-20 20:56:10 +0530

    Education
    Add education details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,61 +25,58 @@ import {
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-black flex">
-      <div className=" flex items-center h-screen w-[400px]">
+    <div className="w-screen h-[1500px] bg-black flex">
+      <div className="sticky top-0 flex items-center h-screen w-[400px]">
         <div className="w-64 h-[500px] ml-7 flex justify-center">
-        
-        <div className="flex items-center flex-col w-full">
-        <h1 className="text-white text-xl">My projects</h1>
-        <div className="border border-white h-12 w-full mt-8 rounded-xl justify-center items-center flex cursor-pointer">
-        <HoverCard>
-          <HoverCardTrigger className="text-white">APEX institue payment portal</HoverCardTrigger>
-          <HoverCardContent className="bg-slate-300 text-black">
-            Payment logg portal for APEX INSTITUTE administrators.
-          </HoverCardContent>
-        </HoverCard>
-        </div>
-        <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
-        <HoverCard>
-          <HoverCardTrigger className="text-white">Workout managemant app</HoverCardTrigger>
-          <HoverCardContent className="bg-slate-300 text-black">
-            Android app that helps keep track of personal workouts.
-          </HoverCardContent>
-        </HoverCard>
-        </div>
-        <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
-        <HoverCard>
-          <HoverCardTrigger className="text-white">Music stream app</HoverCardTrigger>
-          <HoverCardContent className="bg-slate-300 text-black">
-            Music streaming app that stream audio throgh youtube.
-          </HoverCardContent>
-        </HoverCard>
-        </div>
-        <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
-        <HoverCard>
-          <HoverCardTrigger className="text-white">Forest Gaurd</HoverCardTrigger>
-          <HoverCardContent className="bg-slate-300 text-black">
-            Music streaming app that stream audio throgh youtube.
-          </HoverCardContent>
-        </HoverCard>
-        </div>
-      
-        </div>
+          <div className="flex items-center flex-col w-full">
+            <h1 className="text-white text-xl">My projects</h1>
+            <div className="border border-white h-12 w-full mt-8 rounded-xl justify-center items-center flex cursor-pointer">
+              <HoverCard>
+                <HoverCardTrigger className="text-white">APEX institue payment portal</HoverCardTrigger>
+                <HoverCardContent className="bg-slate-300 text-black">
+                  Payment logg portal for APEX INSTITUTE administrators.
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+            <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
+              <HoverCard>
+                <HoverCardTrigger className="text-white">Workout managemant app</HoverCardTrigger>
+                <HoverCardContent className="bg-slate-300 text-black">
+                  Android app that helps keep track of personal workouts.
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+            <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
+              <HoverCard>
+                <HoverCardTrigger className="text-white">Music stream app</HoverCardTrigger>
+                <HoverCardContent className="bg-slate-300 text-black">
+                  Music streaming app that stream audio throgh youtube.
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+            <div className="border border-white h-12 w-full mt-4 rounded-xl justify-center items-center flex cursor-pointer">
+              <HoverCard>
+                <HoverCardTrigger className="text-white">Forest Gaurd</HoverCardTrigger>
+                <HoverCardContent className="bg-slate-300 text-black">
+                  Music streaming app that stream audio throgh youtube.
+                </HoverCardContent>
+              </HoverCard>
+            </div>
+          </div>
         </div>
         <div className="h-[600px] w-0.5 bg-white ml-auto">
         </div>
       </div>
-      <div className="flex flex-col bg-black-500 w-full h-screen ">
-        <div className=" h-[140px] flex items-center justify-center">
-        {/* <Badge variant="outline">Outline</Badge> */}
-        <Menubar className="bg-black">
-        <MenubarMenu>
-        <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">HOME</MenubarTrigger>
-        <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">EDUCATION</MenubarTrigger>
-        <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">PROJECTS</MenubarTrigger>
-        <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">SOCIALS</MenubarTrigger>
-        </MenubarMenu>
-        </Menubar>
+      <div className="flex flex-col bg-black-500 w-full">
+        <div className=" h-[140px] flex items-center justify-center mt-5">
+          <Menubar className="bg-black">
+            <MenubarMenu>
+              <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">HOME</MenubarTrigger>
+              <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">EDUCATION</MenubarTrigger>
+              <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">PROJECTS</MenubarTrigger>
+              <MenubarTrigger className="bg-black text-white hover:underline cursor-pointer">SOCIALS</MenubarTrigger>
+            </MenubarMenu>
+          </Menubar>
         </div>
         <h1 className="headTxt">&lt;Hi,I'm <span className="text-yellow-500 font-bold font-mono">Kavindu Perera</span>/&gt;</h1>
         <Image
@@ -90,19 +87,23 @@ export default function Home() {
         height={250}
         width={250}
         />
-        {/* <h1 className="text-white mt-5 ml-40 text-5xl font-mono"><span className="text-yellow-500 font-bold font-mono">Kavindu Perera</span>/&gt;</h1> */}
-        {/* <div className="h-[150px] md:w-[600px] ml-20 mt-10">
-          <p className="text-white text-lg font-mono">&lt;"I’m dedicated to honing my skills and delivering high-quality work. I believe in the power of persistence and focus, always striving to improve with each project I take on. Without gibbering on more, let me show you what I’ve done."/&gt;</p>
-        </div> */}
         <div className="para">
           <p className="text-white text-lg font-mono">I’m passionate about crafting innovative solutions and constantly improving my skills. With every project, I aim for excellence and growth. Ready to explore my work?</p>
         </div>
-        <div id="buttonBox" className="md:w-[700px] h-[100px] flex items-center justify-center">
+        <div id="buttonBox" className="md:w-[700px] h-[100px] flex items-center justify-center mt-12">
           <div className="justify-center items-center bg-slate-700">
-          <Button variant="secondary">
-            <MoveDown/> Let Me Show You!
-          </Button>
+            <Button variant="secondary">
+              <MoveDown/> Let Me Show You!
+            </Button>
           </div>
+        </div>
+
+        <div className="flex flex-col w-full h-screen mt-20">
+          <h1 className="text-white text-3xl ml-24">College education</h1>
+            <li className="text-white ml-32 mt-10">Royal College</li>
+
+          <h1 className="text-white text-3xl mt-20 ml-24">University education</h1>
+            <li className="text-white ml-32 mt-10">Informatics Institute Of Technology , Colombo</li>
         </div>
       </div>
     </div>
