249f616105862a1c04ea837e9cd83f05a138bd32
Kavindu401 <kavinduperera2211@gmail.com>
2024-12-20 11:41:59 +0530

    rem comments
    remove all comments truble shoot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,22 +29,10 @@ export default function Home() {
       </div>
       <div className="flex flex-col bg-black-500 w-full h-screen ">
         <div className=" h-[140px] flex items-center justify-center">
-        {/* <Badge variant="outline">Outline</Badge> */}
         
         </div>
         <h1 className="headTxt">Hi,I'm <span className="text-yellow-500 font-bold font-mono">Kavindu Perera</span></h1>
         
-         {/* <Image
-              className="absolute right-28 top-60 rounded-full"
-              src="./assets/face.jpg"
-              alt="kavindu perera"
-              width={250}
-              height={250}
-            /> */}
-        {/* <h1 className="text-white mt-5 ml-40 text-5xl font-mono"><span className="text-yellow-500 font-bold font-mono">Kavindu Perera</span>/&gt;</h1> */}
-        {/* <div className="h-[150px] md:w-[600px] ml-20 mt-10">
-          <p className="text-white text-lg font-mono">&lt;"I’m dedicated to honing my skills and delivering high-quality work. I believe in the power of persistence and focus, always striving to improve with each project I take on. Without gibbering on more, let me show you what I’ve done."/&gt;</p>
-        </div> */}
         <div className="para">
           <p className="text-white text-lg font-mono">I’m passionate about crafting innovative solutions and constantly improving my skills. With every project, I aim for excellence and growth. Ready to explore my work?</p>
         </div>
