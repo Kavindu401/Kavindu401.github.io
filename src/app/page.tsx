72d7e03c2284605ee6de4c243ba5f0c55576b74f
Kavindu401 <kavinduperera2211@gmail.com>
2024-12-20 11:51:11 +0530

    styled changes
    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,17 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex">
-      
+      <div className=" flex items-center h-screen w-[400px]">
+        <div className="flex items-center flex-col w-full">
+          <h1 className="text-white text-xl">My projects</h1>
+          {/* <div className="border border-white h-12 w-full mt-8 rounded-xl justify-center items-center flex cursor-pointer">
+
+          </div> */}
+        </div>
+      </div>
+      <div className="flex flex-col bg-black-500 w-full h-screen bg-slate-400">
+
+      </div>
     </div>
   );
 }
