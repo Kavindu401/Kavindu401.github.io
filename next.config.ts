a5151f2aea130d6eef9b37652c631bc5a5f550f7
Kavindu401 <kavinduperera2211@gmail.com>
2024-12-20 18:26:39 +0530

    Image with animation
    Image with animation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization since GitHub Pages doesn't support Next.js image optimization
+  },
 };
 
 export default nextConfig;
